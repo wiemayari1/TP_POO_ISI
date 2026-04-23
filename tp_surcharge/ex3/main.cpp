707640693f471ec7fe6f4d6eb7807dc7a29508a1
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-04-23 23:05:27 +0100

    Implement main function for VoitureAutonome class

---
+++ tp_surcharge/ex3/main.cpp
@@ -0,0 +1,34 @@
+#include "VoitureAutonome.h"
+
+using namespace std;
+
+int main() {
+    VoitureAutonome v1(1, 60.0, 120.0);
+    VoitureAutonome v2(2, 80.0, 200.0);
+
+    cout << "v1: " << v1 << endl;
+    cout << "v2: " << v2 << endl;
+
+	cout << "comparaison v1 et v2 :" << endl;
+    if (v1 == v2) {
+        cout << "meme vitesse et distance" << endl;
+    }
+    else {
+        cout << "different" << endl;
+    }
+
+    if (v1 < v2) {
+        cout << "v1 < v2 (vitesse et distance)" << endl;
+    }
+
+    VoitureAutonome v3 = v1 + 50.0;
+    cout << "v1 + 50: " << v3 << endl;
+
+    cout << "avant ++v1: " << v1 << endl;
+    ++v1;
+    cout << "apres ++v1 (+ 5): " << v1 << endl;
+    v1++;
+    cout << "apres v1++ (+5): " << v1 << endl;
+
+    return 0;
+}
