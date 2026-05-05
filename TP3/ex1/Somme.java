8866df20e448b0017cb15e2339b436cd39c6ebe9
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-05-05 22:27:15 +0100

    Add Somme class to calculate sum of numbers

--- TP3/ex1/Somme.java
+++ TP3/ex1/Somme.java
@@ -1,1 +1,11 @@
+package ex1;
+public class Somme {
+	    public static <T extends Number> double somme(T[] tab, int n) {
+	        double som = 0;
+	        for (int i = 0; i < n; i++) {
+	            som += tab[i].doubleValue();
+	        }
+	        return som;
+	    }
+}
 
